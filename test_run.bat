c900c2b4ef221c2d6ed766eaf16c64402f8c2576
machallboyd <machallboyd@gmail.com>
2023-08-02 12:12:22 -0700

    expand test bat script

--- test_run.bat
+++ test_run.bat
@@ -2,3 +2,9 @@
 
 ECHO Setting up docker api image
 docker build -t api_takehome
+docker run -d --name api_takehome -p 8000:8000 api_takehome
+
+ECHO Activating API
+curl http://127.0.0.1:8000/csv
+
+ECHO Requesting data from local database
