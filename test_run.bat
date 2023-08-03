2a4bbcd483a06249591b3b57fcefb4530e23f19a
machallboyd <machallboyd@gmail.com>
2023-08-02 17:08:24 -0700

    tinker with batch file

--- test_run.bat
+++ test_run.bat
@@ -1,14 +1,18 @@
 #!/bin/bash
-ECHO Setting up docker api image
-docker build -t api_takehome
-docker run -d --name api_takehome -p 8000:8000 api_takehome
+docker network create apinet
 
-ECHO Setting up test db
+echo Setting up test db
 docker pull postgres
-docker run --name test_db -e POSTGRES_PASSWORD=extremely_secure_pw POSTGRES_DB=testdb -d postgres
-curl http://127.0.0.1:8000/setup_test_db
+docker run --name test_postgres -e POSTGRES_PASSWORD=extremely_secure_pw -e POSTGRES_DB=testdb --net apinet -d postgres
 
-ECHO Activating API
-curl http://127.0.0.1:8000/csv
+echo Setting up docker api image
+docker build . -t api_takehome
+docker run -d --name api_takehome --net apinet -p 8000:8000 -e API_DB_PATH=test_postgres api_takehome
+curl http://localhost:8000/setup_test_db
 
-ECHO Requesting data from local database
+./wait-for-it.sh -h 0.0.0.0 -p 8000
+
+echo Activating API
+curl http://localhost:8000/csv
+
+echo Requesting data from local database
