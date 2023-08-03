f0b7ecdf89ab15901c994e76d8b4aca64524c69b
machallboyd <machallboyd@gmail.com>
2023-08-03 12:12:59 -0700

    reformat

--- tests/test_etl.py
+++ tests/test_etl.py
@@ -1,16 +1,27 @@
 import csv
+from datetime import datetime
 import io
 
 import pytest
-
-import api_takehome.app
-from api_takehome.app import csv_cleaner, transform_csvs, load_users, load_report, load_compounds, load_avg_experiments
-from api_takehome.db.experiment_summaries import test_engine, User, ExperimentSummary, Compound, create_test_db
-
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from datetime import datetime
+import api_takehome.app
+from api_takehome.app import (
+    csv_cleaner,
+    load_avg_experiments,
+    load_compounds,
+    load_report,
+    load_users,
+    transform_csvs,
+)
+from api_takehome.db.experiment_summaries import (
+    Compound,
+    create_test_db,
+    ExperimentSummary,
+    test_engine,
+    User,
+)
 
 user_csvs = [
     """
@@ -83,36 +94,43 @@ def test_dir(tmp_path):
 def patch_data_dir(test_dir, monkeypatch):
     monkeypatch.setattr(api_takehome.app, "data_path", test_dir)
 
+
 transformed_csv_data = {
-        "compounds": [
-            ["1", "Compound A", "C20H25N3O"],
-            ["2", "Compound B", "C21H30O2"],
-            ["3", "Compound C", "C8H11NO2"],
-        ],
-        "user_experiments": [
-            ["1", "1", "1;2", "10"],
-            ["2", "1", "2;3", "15"],
-            ["3", "2", "1;3", "20"],
-        ],
-        "users": [
-            ["1", "Alice", "alice@example.com", "2023-01-01"],
-            ["2", "Bob", "bob@example.com", "2023-02-01"],
-        ],
-    }
+    "compounds": [
+        ["1", "Compound A", "C20H25N3O"],
+        ["2", "Compound B", "C21H30O2"],
+        ["3", "Compound C", "C8H11NO2"],
+    ],
+    "user_experiments": [
+        ["1", "1", "1;2", "10"],
+        ["2", "1", "2;3", "15"],
+        ["3", "2", "1;3", "20"],
+    ],
+    "users": [
+        ["1", "Alice", "alice@example.com", "2023-01-01"],
+        ["2", "Bob", "bob@example.com", "2023-02-01"],
+    ],
+}
+
 
 def test_transform_csvs(patch_data_dir):
     result = dict(transform_csvs())
     assert result == transformed_csv_data
 
+
 @pytest.fixture
 def setup_db():
     create_test_db()
 
+
 def test_load_users(patch_data_dir, setup_db):
     result = dict(transform_csvs())
-    load_users(result['users'])
+    load_users(result["users"])
     with Session(test_engine()) as session:
-        result = session.execute(select(User.id, User.name, User.email, User.signup_date)).all()
-    assert list(result) == [tuple((int(row[0]), row[1], row[2], datetime.fromisoformat(row[3]))) for row in transformed_csv_data['users']]
-
-
+        result = session.execute(
+            select(User.id, User.name, User.email, User.signup_date)
+        ).all()
+    assert list(result) == [
+        tuple((int(row[0]), row[1], row[2], datetime.fromisoformat(row[3])))
+        for row in transformed_csv_data["users"]
+    ]
